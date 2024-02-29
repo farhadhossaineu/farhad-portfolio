c910607acfcc3265cb040321d839e743da62f27a
Farhad Hossain <95690124+farhadhossaineu@users.noreply.github.com>
2024-02-29 14:56:32 +0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Contact from './components/contact/Contact';
+import Experience from './components/experience/Experience';
+import Header from './components/header/Header';
+import Intro from './components/intro/Intro';
+import Portfolio from './components/portfolio/Portfolio';
+import Testimonials from './components/testimonials/Testimonials';
+import Topbar from './components/topbar/Topbar';
+
+
+const App = () => {
+  return (
+    <>
+      <Topbar />
+      <Header />
+      <Intro />
+      <Experience />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+    </>
+  )
+}
+
+export default App
